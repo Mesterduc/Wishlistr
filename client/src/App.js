854dfb446255bc1c4ef345a02c493f57a9f19867
Hong Duc Cai <duchongcai@gmail.com>
2021-12-14 00:35:16 +0100

    wish

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Router } from '@reach/router'
+import { Router, navigate } from '@reach/router'
 import Navbar from './Components/Navbar'
 import CreateWish from './Components/CreateWish'
 import Wishes from './views/Wishes/Wishes'
@@ -7,7 +7,7 @@ import Wish from './views/Wish/Wish'
 const API_URL = process.env.REACT_APP_API
 
 function App() {
-  const [wishes, setWishes] = useState([])
+	const [wishes, setWishes] = useState([])
 	useEffect(() => {
 		async function getData() {
 			const url = `${API_URL}/wish`
@@ -17,17 +17,42 @@ function App() {
 		}
 		getData()
 	}, [])
-function getWish(id){
-  return wishes.find(wish  => wish._id = id)
-}
-// console.log(getWish())
+
+	async function postWish(title, link, description) {
+		const data = {
+			Title: title,
+			Link: link,
+			Description: description,
+		}
+		const url = `${API_URL}/wish/`
+		const response = await fetch(url, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+		const reply = await response.json()
+    setWishes([ reply, ...wishes])
+		if (!response.ok) {
+			throw reply
+		}else {
+      navigate("/")
+    }
+   
+	}
+	function getWish(id) {
+		return wishes.find((wish) => (wish._id = id))
+	}
+	// console.log(getWish())
 	return (
 		<>
 			<Navbar />
 			<Router>
 				<Wishes path='/' wishes={wishes}></Wishes>
-        <Wish path='/wish/:id' getWish={getWish}></Wish>
-        <CreateWish path='/createWish'></CreateWish>
+				<Wish path='/wish/:id' getWish={getWish}></Wish>
+				<CreateWish path='/createWish' postWish={postWish}></CreateWish>
 			</Router>
 		</>
 	)
--- client/src/Components/CreateWish.js
+++ client/src/Components/CreateWish.js
@@ -1,9 +1,53 @@
-function createWish() {
+import { useEffect, useState } from 'react'
+const API_URL = process.env.REACT_APP_API
+function CreateWish(props) {
+    const {postWish} = props
+	const [title, setTitle] = useState('')
+	const [link, setLink] = useState('')
+	const [description, setDescription] = useState('')
+    function createWish() {
+		if (title.length > 0) {
+			try {
+				postWish(title, link, description)
+                setTitle('')
+				setLink('')
+				setDescription('')
+			} catch {}
+		} else {
+			// setError('Missing input')
+		}
+	}
+    
 	return (
 		<>
-			<p>create wish</p>
+			<article className="createWish">
+				<p>create wish</p>
+				<input
+					placeholder='Title'
+					onChange={(event) => {
+						setTitle(event.target.value)
+					}}
+					value={title}
+				></input>
+				<input
+					placeholder='Link'
+					onChange={(event) => {
+						setLink(event.target.value)
+					}}
+					value={link}
+				></input>
+				<textarea
+					placeholder='Description'
+					onChange={(event) => {
+						setDescription(event.target.value)
+					}}
+					value={description}
+					rows='5'
+				></textarea>
+                <button className="createWish__button" onClick={() => createWish()}>Create Wish</button>
+			</article>
 		</>
 	)
 }
 
-export default createWish
+export default CreateWish
