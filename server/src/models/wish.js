9940d4265716508b9f01bd709aa22d5253c3fa52
Hong Duc Cai <duchongcai@gmail.com>
2021-12-17 19:28:59 +0100

    data

--- server/src/models/wish.js
+++ server/src/models/wish.js
@@ -55,15 +55,15 @@ async function hej(){
   const db = await Wish.countDocuments({})
   if(db === 0 ){
    let testData = [
-      {Title: "Cykel", Description: "I Wish a bicycle from E-FLY", Link: "www.cykelgear.dk/cykler/elcykler/e-fly-nova-max-iv-n7-centermotor-bla", Comments: [{Text: "What color", Name: "hong"}]},
-      {Title: "Cap", Description: "Colorful cap for the winter", Link: "boozt.com", Comments: [{Text: "What color do you like", Name: "Ip"}, {Text: "What size do you use?", Name: "Ole"}]},
-      {Title: "Bed", Description: "A big comfortable bed", Link: "www.sengeexperten.dk", Comments: [{Text: "What size should the be?", Name: "Morten"}]},
-      {Title: "Apple airpods pro", Description: "For listen to musix", Link: "apple.com", Comments: [{Text: "The pro or is the airpods good enongh", Name: "Emil"}]},
-      {Title: "Computer", Description: "A computer with the new grafic card GTX 3080", Link: "elgiganten.dk", Comments: [{Text: "What abort the CPU", Name: "Ida"}]},
+      {Title: "Cykel", Description: "I Wish a bicycle from E-FLY", Link: "www.cykelgear.dk/cykler/elcykler/e-fly-nova-max-iv-n7-centermotor-bla", CommentCount: 1, Comments: [{Text: "What color", Name: "hong"}]},
+      {Title: "Cap", Description: "Colorful cap for the winter", Link: "boozt.com", CommentCount: 2, Comments: [{Text: "What color do you like", Name: "Ip"}, {Text: "What size do you use?", Name: "Ole"}]},
+      {Title: "Bed", Description: "A big comfortable bed", Link: "www.sengeexperten.dk", CommentCount: 1, Comments: [{Text: "What size should the be?", Name: "Morten"}]},
+      {Title: "Apple airpods pro", Description: "For listen to musix", Link: "apple.com", CommentCount: 1, Comments: [{Text: "The pro or is the airpods good enongh", Name: "Emil"}]},
+      {Title: "Computer", Description: "A computer with the new grafic card GTX 3080", CommentCount: 1, Link: "elgiganten.dk", Comments: [{Text: "What abort the CPU", Name: "Ida"}]},
       {Title: "Gaming Headset", Description: "Headset for gaming", Link: "elgiganten.dk", Comments: []},
-      {Title: "Mice", Description: "Small mice for traveling", Link: "power.dk", Comments: [{Text: "Wires or wireless", Name: "Line"}]},
-      {Title: "Razerblade", Description: "Sharp razerblades for my face", Link: "elgiganten.dk", Comments: [{Text: "For which brand", Name: "Hong"}]},
-      {Title: "Food boxes", Description: "Vegan food box", Link: "www.hellofresh.dk", Comments: [{Text: "How many days a week do you want?", Name: "Cai"}]},
+      {Title: "Mice", Description: "Small mice for traveling", Link: "power.dk", CommentCount: 1, Comments: [{Text: "Wires or wireless", Name: "Line"}]},
+      {Title: "Razerblade", Description: "Sharp razerblades for my face", Link: "elgiganten.dk", CommentCount: 1, Comments: [{Text: "For which brand", Name: "Hong"}]},
+      {Title: "Food boxes", Description: "Vegan food box", Link: "www.hellofresh.dk", CommentCount: 1, Comments: [{Text: "How many days a week do you want?", Name: "Cai"}]},
       {Title: "Smart light", Description: "Smart light that works with the iphone", Link: "power.dk", Comments: []},
     ]
     Wish.insertMany(testData)
